cccf6972101830bee706d19ffa56759565515a85
Pixel Pulse <aquinosherwinjosh@gmail.com>
2024-12-02 18:48:13 +0800

    supabaseAPi

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+
+
+//   Supabase API
+export const supabaseUrl = 'https://mdzeadcxtnqlysicirsb.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kemVhZGN4dG5xbHlzaWNpcnNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxMzQyMzAsImV4cCI6MjA0ODcxMDIzMH0.csrPTlKYdBGKYeEcfFxKpl5xNeLC4Wpxomy4jQaunMw';
